839a936c3af42a935340b4c8fc4bb1222e59034e
Xian Ran <xranthoar@gmail.com>
2013-10-30 22:20:48 +0800

    stmt basic

--- stmt.c
+++ stmt.c
@@ -1,6 +1,36 @@
 #include "stmt.h"
-stmt_t *stmt_init(char *label, wam_op_t op, int narg, ...) { return NULL; }
+#include <string.h>
+#include <stdio.h>
 
-void stmt_destroy(stmt_t *stmt) {}
+stmt_t *stmt_init(char *label, wam_op_t op, int narg, ...)
+{
+	stmt_t *stmt;
+	strcpy(stmt->label, label);
+	stmt->op = op;
 
-void stmt_info(stmt_t *stmt) {}
+	va_list ap;
+	va_start(ap, narg);
+
+	int i;
+	for (i = 0; i < narg; i++) {
+		char *arg = va_arg(ap, char *);
+		stmt->args[i] = malloc(sizeof(arg) + 1);
+		strcpy(stmt->args[i], arg);
+	}
+	stmt->narg = narg;
+	return stmt;
+}
+
+void stmt_destroy(stmt_t *stmt)
+{
+	int i;
+	for (i = 0; i < stmt->narg; i++) {
+		free(stmt->args[i]);
+	}
+	free(stmt);
+}
+
+void stmt_info(stmt_t *stmt)
+{
+	printf("not implemented\n");
+}
